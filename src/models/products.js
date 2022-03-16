a6a79e49356353c1050be462d6b6ebc6175e12cd
le dung <dungltph14568@gmail.com>
2022-03-16 14:21:06 +0700

    datB1

--- src/models/products.js
+++ src/models/products.js
@@ -2,6 +2,23 @@ import mongoose, {ObjectId} from "mongoose";
 
 const productSchema = mongoose.Schema({
     name: {
-        type:
+        type: String,
+        required: true
+    },
+    slug: {
+        type:String,
+        lowercase: true,
+        unique: true,
+        index: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: ObjectId,
+        ref: "Category"
     }
-})
+},{timestamps: true}) 
+
+export default mongoose.model('Product',productSchema);
